451d8c1ecc6ba300a70aff15d2d8b175c132fa08
sim <sim30217@gmail.com>
2024-03-01 22:58:05 +0900

    config

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "my_lib.hpp"
-
+#include "config.hpp"
 int main()
 {
+    std::cout << project_name <<  std::endl;
+    std::cout << project_version << std::endl;
     print_hello_world();
 
     return 0;
